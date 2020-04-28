06a04f1f85a329af4094c7c294d0572f866d31fb
Vishalckc <vishalvermackc@gmail.com>
2020-04-28 21:11:08 +0100

    hoc demo added

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import classes from "./Cockpit.css";
+
+const cockpit = (props) => {
+  useEffect(() => {
+    console.log("[Cockpit.js] useEffect");
+    setTimeout(() => {
+      alert("Data Saved!");
+    }, 1000);
+
+    return () => {
+      
+      console.log("Cockpit.js cleanup work");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("[Cockpit.js] 2nd useEffect");
+    return () => {
+      console.log("Cockpit.js cleanup work in 2nd use effect");
+    };
+  });
+
+  const assignedClasses = [];
+  let btnClass = "";
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
+
+  if (props.personsLength <= 2) {
+    assignedClasses.push(classes.red); // classes = ['red']
+  }
+  if (props.personsLengthh <= 1) {
+    assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(" ")}>This is really working!</p>
+      <button className={btnClass} onClick={props.clicked}>
+        Toggle Persons
+      </button>
+    </div>
+  );
+};
+
+export default React.memo(cockpit);
---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import classes from "./Person.css";
+import Auxiliary from "../../../hoc/Auxiliary";
+
+class Person extends Component {
+  render() {
+    console.log("[Person.js] rendering");
+    return (
+      <Auxiliary>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Auxiliary>
+    );
+  }
+}
+
+export default Person;
